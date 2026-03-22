504ff41e82e2ca3b524186b3c20ab782bfa8c2ee
psaisomya <psaisomya@gmail.com>
2026-03-22 13:57:12 +0530

    Add program to calculate string length

---
+++ Length.c
@@ -0,0 +1,11 @@
+//wap to print the length of a string.
+#include<stdio.h>
+#include<string.h>
+ main()
+{char name[20]="saisomya palai";
+int i=0;
+while(name[i]!='\0')
+{i++;
+}
+printf("the length of the string is %d \n",i);
+}
