c2b720de82d2d5adbdef156cacfeb5f5dac725bc
psaisomya <psaisomya@gmail.com>
2026-03-22 13:55:49 +0530

    Implement pointer access for integer array elements
    Add a program to demonstrate pointer access to an integer array.

---
+++ Int array.c
@@ -0,0 +1,6 @@
+//Explain how you can use a pointer to access and print elements of an integer array. 
+#include <stdio.h>
+int main()
+{int numb[]={10,20,30,40};
+printf("%u\n %u \n %u",numb,&numb,&numb[0]);
+}
