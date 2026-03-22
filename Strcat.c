a672b341501b4285aa9665a1e89246a313483d86
psaisomya <psaisomya@gmail.com>
2026-03-22 14:00:39 +0530

    Implement string concatenation using strcat
    Developed a program to concatenate two strings using strcat.

---
+++ Strcat.c
@@ -0,0 +1,10 @@
+//develope a program to concatenate two strings using strcat.
+#include <stdio.h>
+#include <string.h>
+main()
+{char s1[]="Hello";
+char s2[]="world";
+strcat(s1,s2);
+printf("%s",s1);
+printf("%s",s2);
+}
