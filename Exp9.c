ca6f934fd50af116f5a1137e887301138d0180ef
psaisomya <psaisomya@gmail.com>
2026-05-01 14:26:26 +0530

    Create Exp9.c

---
+++ Exp9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void sortArray(int *arr, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (*(arr + j) > *(arr + j + 1)) {
+                int temp = *(arr + j);
+                *(arr + j) = *(arr + j + 1);
+                *(arr + j + 1) = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22};
+    int n = 5;
+    sortArray(arr, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    return 0;
+}
