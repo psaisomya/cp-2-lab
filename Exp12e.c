60a77d815a8a98c6daeb1666fe4386f6ced52d6c
psaisomya <psaisomya@gmail.com>
2026-05-01 14:17:23 +0530

    Add program to print command line arguments

---
+++ Exp12e.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Arguments passed:\n");
+    for (int i = 0; i < argc; i++) {
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+    return 0;
+}
